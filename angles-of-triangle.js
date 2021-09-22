30584a8dd54440ecdb4247da2b5cab3c8620d25f
Arpita Dasgupta <dasguptaarpita1999@gmail.com>
2021-09-22 20:13:55 +0530

    is Triangle Javascript

---
+++ angles-of-triangle.js
@@ -0,0 +1,23 @@
+const inputs = document.querySelectorAll(".angle-input");
+const isTriangleBtn = document.querySelector("#is-triangle-btn");
+const outputEl = document.querySelector(".output");
+
+function sumOfAngles(angle1, angle2, angle3) {
+  const sum = angle1 + angle2 + angle3;
+  return sum;
+}
+
+function isTriangle() {
+  const sum = sumOfAngles(
+    Number(inputs[0].value),
+    Number(inputs[1].value),
+    Number(inputs[2].value)
+  );
+  if (sum === 180) {
+    outputEl.innerText = "These angles form a Triangle.";
+  } else {
+    outputEl.innerText = "No, This doesn't form a triangle";
+  }
+}
+
+isTriangleBtn.addEventListener("click", isTriangle);
